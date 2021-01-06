2ef3cd4979fde829f77b4b9191c36d1f755f6349
Leonardo <imrealleonardo@gmail.com>
2021-01-06 10:51:33 +0800

    -polish raw url;

--- src/consts.ts
+++ src/consts.ts
@@ -1,9 +1,9 @@
-
 /** 数据文件url前缀 */
-export const RAW_JSON_BASE_URL = "https://raw.githubusercontent.com/realLeonardo/v2ex-daily-hot-topic/master/raw/"
+export const RAW_JSON_BASE_URL =
+  "https://realleonardo.github.io/v2ex-daily-hot-topic/raw/";
 
 /** 数据爬虫开始日期: "2020/12/18" */
-export const SCRIPT_BEGIN_TIMESTAMP: number = 1608220800000
+export const SCRIPT_BEGIN_TIMESTAMP: number = 1608220800000;
 
 /** 一天的毫秒数 */
 export const DAY_MILLISECONDS_COUNT: number = 24 * 60 * 60 * 1000;
